14fee6970f2315d9a87d9b16be7ba01dbb116ebe
Li Yishan <hycosti@gmail.com>
2024-02-29 19:19:05 +0800

    Update quartz.config.ts
    change page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Li Yishan",
+    pageTitle: "Yishan",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
